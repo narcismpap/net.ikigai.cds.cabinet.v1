82bd6a30fc692a4d4604185ffbb5e108722289a0
Valence <office@valencex.com>
2018-12-08 15:45:30 +0800

    Initial Cabinet service def

---
+++ cabinet.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"flag"
+	"google.golang.org/grpc"
+	"github.com/golang/protobuf/proto"
+)
+
+var (
+	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile   = flag.String("cert_file", "", "The TLS cert file")
+	keyFile    = flag.String("key_file", "", "The TLS key file")
+	jsonDBFile = flag.String("json_db_file", "testdata/route_guide_db.json", "A json file containing a list of features")
+	port       = flag.Int("port", 10000, "The server port")
+)
