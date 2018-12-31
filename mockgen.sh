721e16d9727497b3bc7e5a733d3b890402191469
Valence <office@valencex.com>
2018-12-31 15:02:47 +0800

    Testing gomock; Not applicable to server

---
+++ mockgen.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mockgen cds.ikigai.net/cabinet.v1/rpc CDSCabinetClient > src/cds.ikigai.net/cabinet.v1/mock/cds_mock.go
---
+++ server/main_test.go
@@ -0,0 +1,23 @@
+package server_test
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+)
+
+type rpcMsg struct {
+	msg proto.Message
+}
+
+func (r *rpcMsg) Matches(msg interface{}) bool {
+	m, ok := msg.(proto.Message)
+	if !ok {
+		return false
+	}
+	return proto.Equal(m, r.msg)
+}
+
+func (r *rpcMsg) String() string {
+	return fmt.Sprintf("is %s", r.msg)
+}
+
